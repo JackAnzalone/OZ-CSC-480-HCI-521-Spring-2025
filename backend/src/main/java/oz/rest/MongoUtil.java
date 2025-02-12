9d959f428be7a5fee53fd0617bda67b2017542bf
Jack <98469947+JackAnzalone@users.noreply.github.com>
2025-02-12 11:29:59 -0500

    Update MongoUtil.java

--- backend/src/main/java/oz/rest/MongoUtil.java
+++ backend/src/main/java/oz/rest/MongoUtil.java
@@ -15,8 +15,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ApplicationScoped
 public class MongoUtil {
-    private static final String CONNECTION_STRING = "mongodb+srv://database_admin:pXzO2cMkmk7LXVCH@csc480cluster.ldmco.mongodb.net/";
-    private static final String DATABASE_NAME = "Data";
+    private static final String CONNECTION_STRING = "";
+    private static final String DATABASE_NAME = "";
     private static MongoClient mongoClient;
     private static MongoDatabase database;
 
